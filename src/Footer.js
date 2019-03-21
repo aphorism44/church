252e7bb5fbf05411b5a72af753d0528ad9b9561d
aphorism44 <dom.jesse6@gmail.com>
2019-03-21 13:45:32 -0500

    holy week 2019 updates; new ico

--- src/Footer.js
+++ src/Footer.js
@@ -9,7 +9,6 @@ class Footer extends Component {
         <br/>137 West Main Street
         <br/>Merrimac, WI  53561
         <br/>(608) 493-2640
-        <br/><a href="mailto:immluth@merr.com">Immluth@merr.com</a>
         <br/><br/><a href="https://www.google.com/maps/place/137+Main+St,+Merrimac,+WI+53561/@43.3733374,-89.6268227,17z/data=!3m1!4b1!4m5!3m4!1s0x8807117df261704d:0xcf4758a8eb3bcb37!8m2!3d43.3733335!4d-89.624634">Map</a>
       </footer>
     );
